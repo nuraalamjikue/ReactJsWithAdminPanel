072f07111c2fcf7c61cf67079dbab90f92236d22
nuraalamjikue <nuraalamjikue445@gmail.com>
2024-09-03 09:53:40 +0600

    Updates

---
+++ assets/getRTLPlacement-K8-ctlnc.js
@@ -0,0 +1,1 @@
+import{t as e}from"./DefaultLayout-D7MR_y7B.js";var a=function(t,r){switch(t){case"right":return e(r)?"left":"right";case"left":return e(r)?"right":"left";default:return t}};export{a as g};
